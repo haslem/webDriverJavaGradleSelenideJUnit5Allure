32a0b47b3c5a98d954eaedce73a503a67b62c411
Yauhen.Masalkou <haslem11@gmail.com>
2021-02-24 13:34:13 +0100

    check annotations

--- src/test/java/notWeb/CountTest.java
+++ src/test/java/notWeb/CountTest.java
@@ -1,17 +1,21 @@
 package notWeb;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CountTest {
 
-    Count testCount = new Count(4, 5);
+    Count testCount;
+
+    @BeforeEach
+    public void setup(){
+        System.out.println("testing instance is created");
+        testCount = new Count(4, 5);
+    }
 
     @Test
     void sumArbitraryNumbers() {
-
         Assertions.assertEquals(19, testCount.sumArbitraryNumbers(10, 9));
     }
 
@@ -20,7 +24,15 @@ class CountTest {
         Assertions.assertEquals(9, testCount.sumBuiltInNumbers());
     }
 
+    @AfterEach
+    public void afterEach(){
+        System.out.println("After each");
+    }
 
+    @AfterAll
+    public static void afterAll(){
+        System.out.println("After all");
+    }
 
 
 }
